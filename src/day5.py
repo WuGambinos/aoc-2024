2ff170b57367e09cdaf92276786abef58745e89f
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2024-12-05 02:55:53 -0500

    Day5 completed

--- src/day5.py
+++ src/day5.py
@@ -1,17 +1,11 @@
 from helper import read_file_to_list
+from functools import cmp_to_key
 
 lines = read_file_to_list("../inputs/day5.txt")
 
-valid_updates = []
 ordering = [] 
 updates = []
 m = {}
-
-class PageOrder:
-    def __init__(self, before, after):
-        self.before = before 
-        self.after = after
-
 
 def setup():
     empty_line_seen = False
@@ -30,8 +24,8 @@ def setup():
         before = int(data[0])
         after = int(data[1])
         if (before not in m):
-            lst = [after]
-            m[before] = lst
+            temp_list = [after]
+            m[before] = temp_list
         else:
             m[before].append(after)
 
@@ -44,45 +38,66 @@ def check(update, value):
     return True
 
 def valid_page_update(update):
-    nums = update.split(",")
-    nums = [int(n) for n in nums]
 
-    for i in range(len(nums)):
-        if nums[i] not in m and i == len(nums) - 1:
+    for i in range(len(update)):
+        if update[i] not in m and i == len(update) - 1:
             return True
 
-        elif nums[i] not in m:
-            return check(nums, nums[i])
+        elif update[i] not in m:
+            return check(update, update[i])
 
-        order_list = m[nums[i]]
-        for j in range(i+1, len(nums)):
-            if nums[j] not in order_list:
+        order_list = m[update[i]]
+        for j in range(i+1, len(update)):
+            if update[j] not in order_list:
                 return False
     return True
 
 
-def solve():
-    for update in updates:
-        valid = valid_page_update(update)
-        print(update, valid)
-        if (valid):
-            valid_updates.append(update)
-    print(valid_updates)
-
-    nums_2d = []
-    for valid in valid_updates:
-        nums = valid.split(",")
-        nums = [int(n) for n in nums]
-        nums_2d.append(nums)
-
+def sum_middle(updates):
     res = 0
-    for valid in nums_2d:
-        if len(valid) % 2 == 0 :
-            res += valid[len(valid) // 2 - 1]
+    for update in updates:
+        if len(update) % 2 == 0 :
+            res += update[len(update) // 2 - 1]
         else:
-            res += valid[len(valid) // 2]
+            res += update[len(update) // 2]
+    return res
 
-    print("DAY 1: ", res)
+def custom_cmp(x, y):
+    if x in m:
+        order_list = m[x]
+        if y in order_list:
+            return -1
+        else:
+            return 1
+    else:
+        return 1
+
+def fix_order(invalid_updates):
+    cmp_key  = cmp_to_key(custom_cmp)
+    for invalid in invalid_updates:
+        invalid.sort(key=cmp_key)
+
+
+def solve():
+    valid_updates = []
+    invalid_updates = []
+
+    for update in updates:
+        nums = update.split(",")
+        nums = [int(n) for n in nums]
+        valid = valid_page_update(nums)
+        if (valid):
+            valid_updates.append(nums)
+        else:
+            invalid_updates.append(nums)
+
+    day1 = sum_middle(valid_updates)
+
+    fix_order(invalid_updates)
+    day2 = sum_middle(invalid_updates)  
+
+    print("PART 1: ", day1)
+    print("PART 2: ", day2)
 
 setup()
 solve()
