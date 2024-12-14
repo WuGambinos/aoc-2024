bb11bdfd9d2616dc8a14f7c0d1837de3475f46d5
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2024-12-14 08:22:10 -0500

    Day13 completed

--- src/day13.py
+++ src/day13.py
@@ -1,13 +1,9 @@
 from helper import read_file_to_string
 import sympy as sp
 
-
-
-
-
-
-
-f = open('../inputs/day13.txt', 'r')
+CONVERSION = 10000000000000
+A_TOKEN_COST = 3
+B_TOKEN_COST = 1
 
 
 class Prize:
@@ -27,48 +23,17 @@ class Button:
     def print(self):
         print(f"LABEL {self.label} X: {self.dx} Y: {self.dy}")
 
-"""
-def recurse(acc,  n1, n2, visited, count_n1, count_n2):
-
-    if acc == 0:
-        return count_n1, count_n2
-
-    if acc  < 0:
-        return None
-
-    if acc in visited:
-        return None
-
-    visited.add(acc)
-
-    result_n1 = recurse(acc - n1, n1, n2, visited, count_n1+1, count_n2)
-    if result_n1:
-        return result_n1
-
-
-    result_n2 = recurse(acc - n2, n1, n2, visited, count_n1, count_n2+1)
-    if result_n2:
-        return result_n2
-
-    visited.remove(acc)
-
-    return None
-"""
-
-
-def solve():
+def solve(part2):
     turns = []
     inner = []
 
     buttons = []
     claw_machines = {}
 
-    A_TOKEN_COST = 3
-    B_TOKEN_COST = 1
 
+    f = open('../inputs/day13.txt', 'r')
     for line in f:
         if not line.strip() :
-            print("EMPTY")
             turns.append(inner)
             inner = []
         else:
@@ -100,10 +65,13 @@ def solve():
     total_cost = 0
 
     for (p, buttons) in claw_machines.items():
-        p.print()
         b1, b2 = buttons[0], buttons[1]
         x1, x2 = b1.dx, b2.dx
         y1, y2 = b1.dy, b2.dy
+
+        if part2:
+            p.x += CONVERSION
+            p.y += CONVERSION
 
         eq1 = x1 * x + x2 * y - p.x
         eq2 = y1 * x + y2 * y - p.y
@@ -114,15 +82,14 @@ def solve():
         if type(total_x) == sp.core.numbers.Integer and type(total_y) == sp.core.numbers.Integer:
             cost = total_x * A_TOKEN_COST + B_TOKEN_COST * total_y
             total_cost += cost
-            """
-            print(total_x, total_y)
-            print("COST", cost)
-            print()
-            """
 
-    print("PART 1", total_cost)
-    
+    if part2:
+        print("PART 2", total_cost)
+    else:
+        print("PART 1", total_cost)
 
-solve()
+solve(False)
+solve(True)
+
 
 
